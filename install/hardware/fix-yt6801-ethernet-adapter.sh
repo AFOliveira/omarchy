efa476114bbef902afc4d0658808b44f4aa1c66b
Afonso Oliveira <afonso.oliveira707@gmail.com>
2026-09-06 21:46:19 +0100

    Verify YT6801 cutover before retiring the vendor driver

--- install/hardware/fix-yt6801-ethernet-adapter.sh
+++ install/hardware/fix-yt6801-ethernet-adapter.sh
@@ -1,4 +1,9 @@
 # Use the upstream driver for the Motorcomm YT6801 adapter used by the Slimbook Executive.
-if [[ -n $(lspci -Dn -d 1f0a:6801) ]]; then
+if ! yt6801_devices=$(lspci -Dn -d 1f0a:6801); then
+  echo "Unable to discover YT6801 adapters." >&2
+  return 1
+fi
+
+if [[ -n $yt6801_devices ]]; then
   omarchy-pkg-drop yt6801-dkms
 fi
